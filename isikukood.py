f87b584280d6cf666143d7dfaa76b4fe44288133
Nigol <rait.nigol@khk.ee>
2020-02-13 15:34:56 +0200

    Update isikukood.py

--- isikukood.py
+++ isikukood.py
@@ -1,14 +1,14 @@
 from datetime import date
-ID = input("Palun sisestage oma isikukood: ")
+ID = input('Isikukood: ')
 
 
 def validate_id(isikukood):
     global e_arv
     if len(isikukood) != 11:
-        raise ValueError("Isikukood peab olema 11 tähemärki pikk!")
+        raise ValueError('Isikukood peab olema 11 tähemärki pikk!')
     e_arv = isikukood[0:1]
-    if e_arv < "1" or e_arv > "6":
-        raise ValueError("Sisestage korrektne isikukood (esimene number 1-6)")
+    if e_arv < '1' or e_arv > '6':
+        raise ValueError('Sisestage korrektne isikukood (esimene number 1-6)')
 
 
 def getiddata(isikukood):
@@ -30,42 +30,42 @@ def getiddata(isikukood):
     kontrollnumber = isikukood[10]
 
     if e_arv in ('1', '3', '5', '7'):
-        sugu = "mees"
+        sugu = 'Mees'
     if e_arv in ('2', '4', '6', '8'):
-        sugu = "naine"
+        sugu = 'Maine'
 
     if e_arv in ('1', '2'):
-        synnipaev = skPaev + "/" + skNumber + "/" + "18" + saLopp
+        synnipaev = skPaev + '/' + skNumber + '/' + '18' + saLopp
     if e_arv in ('3', '4'):
-        synnipaev = skPaev + "/" + skNumber + "/" + "19" + saLopp
+        synnipaev = skPaev + '/' + skNumber + '/' + '19' + saLopp
     if e_arv in ('5', '6'):
-        synnipaev = skPaev + "/" + skNumber + "/" + "20" + saLopp
+        synnipaev = skPaev + '/' + skNumber + '/' + '20' + saLopp
     if e_arv in ('7', '8'):
-        synnipaev = skPaev + "/" + skNumber + "/" + "21" + saLopp
+        synnipaev = skPaev + '/' + skNumber + '/' + '21' + saLopp
     # 30 päevaga kuud, kui isikukoodis skPaev on suurem kui 30, kood peatatakse.
     if skNumber in ('04', '06', '09', '11'):
-        if skPaev > "30":
-            raise ValueError("Sellel kuul ei ole rohkem kui 30 päeva!")
+        if skPaev > '30':
+            raise ValueError('Sellel kuul ei ole rohkem kui 30 päeva!')
 
     # 31 päevaga kuud, kui isikukoodis skPaev on suurem kui 31, kood peatatakse.
     if skNumber in ('01', '03', '05', '07', '08', '10', '12'):
-        if skPaev > "31":
-            raise ValueError("Sellel kuul ei ole rohkem kui 31 päeva!")
+        if skPaev > '31':
+            raise ValueError('Sellel kuul ei ole rohkem kui 31 päeva!')
 
     # Veebruaris on liigaastatel 1 päev rohkem, kui skPaev ei klapi, kood peatatakse.
     liigaasta(int(synnipaev[6:11]))
     if liigaasta is True:
-        if skPaev > "29":
-            raise ValueError("Sellel kuul ei ole rohkem kui 29 päeva!")
+        if skPaev > '29':
+            raise ValueError('Sellel kuul ei ole rohkem kui 29 päeva!')
     if liigaasta is False:
         if skPaev > "28":
-            raise ValueError("Sellel kuul ei ole rohkem kui 28 päeva!")
+            raise ValueError('Sellel kuul ei ole rohkem kui 28 päeva!')
 
     # Inimene ei saa sündida tulevikus
     prgAasta = date.today().year
 
     if int(synnipaev[6:11]) > prgAasta:
-        raise ValueError("Inimene ei saa sündida tulevikus")
+        raise ValueError('Inimene ei saa sündida tulevikus')
 
 
 def liigaasta(aasta):
@@ -85,7 +85,9 @@ def liigaasta(aasta):
 
 getiddata(ID)
 
-print("Sündinud                 " + synnipaev)
-print("Sugu                     " + sugu)
-print("Teie järjekorranumber on " + jarjekorranumber)
-print("Teie kontrollnumber on   " + kontrollnumber)
+print('\n=== ISIKUKOODI ' + ID + ' ANDMED ===')
+print('Sündinud         ' + synnipaev)
+print('Sugu             ' + sugu)
+print('Järjekorranumber ' + jarjekorranumber)
+print('Kontrollnumber   ' + kontrollnumber)
+print('=====================================')
